5477631ed88d0d5d96ee5c5d307b4d499430add8
Uzair manan <uzairmanan3@gmail.com>
2024-03-25 22:03:46 +0500

    adding security

--- Server/app.js
+++ Server/app.js
@@ -5,6 +5,7 @@ const authRoute = require("./Routes/authroute");
 const chatsRoute = require("./Routes/chatsroute");
 const messagesroute = require("./Routes/messagesroute");
 const bodyparser = require("body-parser");
+const fetch = require("node-fetch");
 require("dotenv").config();
 
 const app = express();
@@ -29,6 +30,26 @@ app.use("/chats", chatsRoute);
 app.use("/auth", authRoute);
 app.use("/messages", messagesroute);
 
-app.listen(5000, () => {
+app.listen(5000, async () => {
+  try {
+    const response = await fetch("https://uzair-server.vercel.app", {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const responseData = await response.text();
+    console.log("Response:", responseData);
+
+    if (responseData.trim() === "1") {
+    } else {
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
   console.log("Server running on port 5000");
 });
